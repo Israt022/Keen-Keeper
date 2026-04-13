57b1186ef07309f5fb3e5149e17d7cd1bccea5a7
Israt Jahan <eraisrat022@gmail.com>
2026-04-14 00:49:47 +0600

    Navbar Added

--- src/component/shared/Navbar.jsx
+++ src/component/shared/Navbar.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import { IoTimeOutline } from 'react-icons/io5';
+import { PiChartLineLight } from 'react-icons/pi';
+import { RiHome2Line } from 'react-icons/ri';
+import { NavLink } from 'react-router';
+import logo from '../../assets/images/logo.png'
 
 const Navbar = () => {
+    const links = <>
+        <li><NavLink to='/' className={({isActive}) => `font-semibold btn border-none flex item-center ${isActive ? 'bg-green-900 text-white' : 'text-gray-500'}`}><RiHome2Line size={20}/> Home</NavLink></li>
+        <li><NavLink to='/timeline' className={({isActive}) => `font-semibold btn border-none flex item-center ${isActive ? 'bg-green-900 text-white' : 'text-gray-500'}`}><IoTimeOutline size={20}/> Timeline</NavLink></li>
+        <li><NavLink to='/status' className={({isActive}) => `font-semibold btn border-none flex item-center ${isActive ? 'bg-green-900 text-white' : 'text-gray-500'}`}><PiChartLineLight size={20}/> Status</NavLink></li>
+    </>
     return (
-        <div className="navbar bg-base-100 shadow-sm">
-            <div className="flex-1">
-                <a className="btn btn-ghost text-xl">daisyUI</a>
-            </div>
-            <div className="flex-none">
-                <ul className="menu menu-horizontal px-1">
-                    <li className='btn'><a>Link</a></li>
-                    <li className='btn'>
-                        Link2
-                    </li>
-                </ul>
+        <div className='bg-base-100 shadow-sm '>
+            <div className="navbar container mx-auto">
+                <div className="flex-1">
+                    <NavLink to='/' className="text-xl font-bold">
+                    {/* Keen<span className='text-gray-600 font-semibold'>Keeper</span> */}
+                    <img src={logo} alt="" />
+                </NavLink>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1 flex-wrap">
+                        {links}
+                    </ul>
+                </div>
             </div>
         </div>
     );
--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router';
 import Root from '../layout/Root';
 import Homepage from '../pages/HomePage/Homepage';
+import TimeLine from '../pages/Timeline/TimeLine';
+import Status from '../pages/Status/Status';
 
 export const router = createBrowserRouter([
   {
     path : '/',
     Component : Root,
     children : [
-        {index : true, Component : Homepage}
+        {index : true, Component : Homepage},
+        {
+            path : '/timeline',
+            Component : TimeLine
+        },
+        {
+            path : '/status',
+            Component : Status
+        },
     ]
   }
 ])
